a5b6922b9409c74032beb00dc7c37a041b31e836
Omsurvase24 <omsurvase24@gmail.com>
2024-02-05 23:10:12 +0530

    Subscribers

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.html import mark_safe
-
+from django.contrib.auth.models import User
 # Banners
 
 
@@ -97,3 +97,34 @@ class SubPlanFeature(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PlanDiscount(models.Model):
+    subplan = models.ForeignKey(SubPlan, on_delete=models.CASCADE)
+    total_months = models.IntegerField()
+    total_discount = models.IntegerField()
+
+    def __str__(self):
+        return str(self.total_months)
+
+
+class Subscriber(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=20)
+    address = models.TextField()
+    img = models.ImageField(upload_to="subs/")
+
+    def __str__(self):
+        return self.user
+
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="80" />' % (self.img.url))
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plan = models.ForeignKey(SubPlan, on_delete=models.CASCADE)
+    price = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.plan)
